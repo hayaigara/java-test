d4a14f867444de25609c73f64c94c54622342df9
hayaigara <145536627+hayaigara@users.noreply.github.com>
2025-01-14 22:49:40 +0900

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -4,6 +4,24 @@ package work;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.print("Hello,world!");
+		System.out.println("Hello,world!");
+		
+		String Hello = "こんにちは、皆さん";
+		
+		System.out.println(Hello);
+		
+		int old;
+		
+		String name;
+		
+		old = 31;
+		
+		name = "hayato";
+		
+		System.out.println(old + "歳です、" + name + "と読みます。");
+		
+		
+		
+		
 	}
 }
